9899f9f366ec82cfb5f380e1c26a7058bb6a3846
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-05-14 21:19:57 -0300

    refactor: Update search form to use controlled input and improve search functionality

--- src/components/search-form.tsx
+++ src/components/search-form.tsx
@@ -3,42 +3,58 @@ import { Search } from "lucide-react"
 
 // Components
 import { usePathname, useRouter, useSearchParams } from "next/navigation"
-import { useDebouncedCallback } from "use-debounce"
+import { ChangeEvent, FormEvent, useState } from "react"
 
 export const SearchForm = () => {
+  const [term, setTerm] = useState("")
   const searchParams = useSearchParams()
   const pathname = usePathname()
   const path = pathname.includes("/app") ? "/app" : pathname
 
   const router = useRouter()
-  const handleSearch = useDebouncedCallback((term: string) => {
+
+  const handleSearch = (e: FormEvent) => {
+    e.preventDefault()
     const params = new URLSearchParams(searchParams)
 
-    if (term) {
-      if (params.has("page")) params.delete("page")
+    if (term.trim() !== "") {
+      if (params.has("page")) params.delete("search")
       params.set("search", term)
+    }
+
+    router.replace(`${path}?${params.toString()}`)
+  }
+
+  const handleChangeInputValue = (e: ChangeEvent<HTMLInputElement>) => {
+    const params = new URLSearchParams(searchParams)
+    if (e.target.value.trim() !== "") {
+      setTerm(e.target.value)
     } else {
       params.delete("page")
       params.delete("search")
+      setTerm("")
     }
-    router.replace(`${path}?${params.toString()}`)
-  }, 300)
+    router.replace(`${path}`)
+  }
 
   return (
     <div className="w-full md:w-2/3 lg:w-1/2">
-      <div className="relative h-9 overflow-hidden rounded-md">
+      <form
+        className="relative h-9 overflow-hidden rounded-md"
+        onSubmit={handleSearch}
+      >
         <input
           type="search"
           name="search"
           placeholder="Pesquisar"
           className="h-full w-full border-0 pl-10 pr-2 outline-none ring-0"
-          onChange={(e) => handleSearch(e.target.value)}
-          defaultValue={searchParams.get("search")?.toString()}
+          onChange={handleChangeInputValue}
+          value={term}
         />
         <button className="top absolute left-2 top-0 size-9 text-muted-foreground">
           <Search />
         </button>
-      </div>
+      </form>
     </div>
   )
 }
