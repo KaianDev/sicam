ac7685048d2c351fd462eb8a22f8307666c11b05
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-05-14 22:57:26 -0300

    fix: include add box action

--- src/actions/box.ts
+++ src/actions/box.ts
@@ -83,3 +83,64 @@ export const fetchBoxes = async ({ page, search }: SearchParams) => {
     pageCount,
   }
 }
+
+export const addNewBox = async (data: CreateOrUpdateBoxType) => {
+  // const ownerId = "55c511dc-ec7c-4a3c-82e6-01d99df846d0" // John Snow
+  const ownerId = "6d578917-1d2d-4e09-8474-b7f59de71363" // Daenarys Targeryen
+
+  try {
+    const user = await prisma.user.findFirst({ where: { id: ownerId } })
+
+    if (!user) {
+      return { message: "Usuário não encontrado!" }
+    }
+
+    const sectorId = user.sectorId
+    const { content, entityId, observation } = data
+
+    const entity = await prisma.entity.findFirst({
+      where: { id: entityId },
+      include: { boxes: true },
+    })
+
+    const sector = await prisma.sector.findFirst({
+      where: {
+        id: sectorId,
+      },
+      include: {
+        boxes: {
+          where: {
+            entityId,
+          },
+        },
+      },
+    })
+
+    if (!entity) {
+      return { message: "A entidade não foi encontrada" }
+    }
+
+    if (!sector) {
+      return { message: "Setor inválido" }
+    }
+
+    const numBox = sector.boxes.length + 1
+
+    await prisma.box.create({
+      data: {
+        content,
+        observation,
+        numBox,
+        entityId,
+        ownerId,
+        sectorId,
+      },
+    })
+  } catch (error) {
+    return { message: "Erro de BD: Falha ao criar caixa" }
+  }
+
+  revalidatePath("/")
+  revalidatePath("/app")
+  redirect("/app")
+}
