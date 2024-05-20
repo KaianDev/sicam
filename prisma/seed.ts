4e7ebf77b162984b638437f46ecf6c28b705343b
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-05-19 23:16:16 -0300

    chore: Add seed file and prisma  seeding script to package.json

---
+++ prisma/seed.ts
@@ -0,0 +1,42 @@
+import bcrypt from "bcrypt"
+
+import { PrismaClient, Sector, User } from "@prisma/client"
+const prisma = new PrismaClient()
+
+const main = async () => {
+  console.log("Iniciando seed")
+
+  const dataSector = { name: "ROOT" } as Sector
+  console.log("Criando setor ROOT")
+
+  const sector = await prisma.sector.create({ data: dataSector })
+  console.log("Setor ROOT criado com sucesso", sector)
+
+  const name = process.env.SICAM_ADMIN_NAME || "Admin"
+  const email = process.env.SICAM_ADMIN_EMAIL || "admin@sicam.com"
+  const password = process.env.SICAM_ADMIN_PASSWORD || "admin123"
+
+  const hashedPassword = await bcrypt.hash(password, 10)
+  const dataAdminUser = {
+    name,
+    email,
+    password: hashedPassword,
+    role: "ADMIN",
+    sectorId: sector.id,
+  } as User
+
+  console.log("Criando usuário administrador")
+  const adminUser = await prisma.user.create({ data: dataAdminUser })
+
+  console.log("Usuário administrador criado com sucesso", adminUser)
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
---
+++ src/types/env.d.ts
@@ -0,0 +1,7 @@
+declare module NodeJS {
+  interface ProcessEnv {
+    SICAM_ADMIN_NAME: string
+    SICAM_ADMIN_EMAIL: string
+    SICAM_ADMIN_PASSWORD: string
+  }
+}
