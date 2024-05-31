9fdaa0f3c3d0694ebad4ea8e9146d379c9d85f1d
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-05-30 22:32:09 -0300

    chore: firebase setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { hostname: "firebasestorage.googleapis.com", protocol: "https" },
+    ],
+  },
+}
 
 export default nextConfig
--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -3,5 +3,11 @@ declare module NodeJS {
     SICAM_ADMIN_NAME: string
     SICAM_ADMIN_EMAIL: string
     SICAM_ADMIN_PASSWORD: string
+    FIREBASE_API_KEY: string
+    FIREBASE_AUTH_DOMAIN: string
+    FIREBASE_PROJECT_ID: string
+    FIREBASE_STORAGE_BUCKET: string
+    FIREBASE_MESSAGING_SENDER_ID: string
+    FIREBASE_APP_ID: string
   }
 }
